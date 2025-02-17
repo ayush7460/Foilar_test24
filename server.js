2ae6dcf2493f3404bb68ff3608772d5cb080365d
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-02-17 22:53:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.use('/uploads', express.static('uploads'));
 
 app.use(express.json());
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+// app.use(express.json({ limit: "10mb" }));
+// app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 
 // Start the server
