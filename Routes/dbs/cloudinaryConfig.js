0ee57e6dcbbc45d4b459d4a376be08bd232c9b5e
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-02-17 20:55:04 +0530

    .

--- Routes/dbs/cloudinaryConfig.js
+++ Routes/dbs/cloudinaryConfig.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dvopqpu5k",
-  api_key: "513979796987495",
-  api_secret: "D9_QhGim0a4O2CYhzvF7Ur1Br2I",
+  cloud_name: process.env.Cloud_name || "dvopqpu5k",
+  api_key: process.env.Api_key || "513979796987495",
+  api_secret: process.env.Api_secre || "D9_QhGim0a4O2CYhzvF7Ur1Br2I",
 });
 
 module.exports = cloudinary;
