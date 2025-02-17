f065479f343dfab63aba7183b4dd6f5dd2d228b0
ayush gupta <143194898+ayush7460@users.noreply.github.com>
2025-02-17 21:59:06 +0530

    Update cloudinaryConfig.js

--- Routes/dbs/cloudinaryConfig.js
+++ Routes/dbs/cloudinaryConfig.js
@@ -2,8 +2,8 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dvopqpu5k",
-  api_key: "513979796987495",
-  api_secret: "D9_QhGim0a4O2CYhzvF7Ur1Br2I",
+  api_key: "556568254341843",
+  api_secret: "Ov7zwbx5F6cJYB76sQkhCIYbWTo",
 });
 
 module.exports = cloudinary;
